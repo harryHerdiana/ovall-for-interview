1dad791ab3e6a0a805fd595bede2cc897ccf1276
hellersson <dev@dominikheller.de>
2022-02-20 22:59:09 +0100

    test env

--- modules/shopify/api/storefront/client.ts
+++ modules/shopify/api/storefront/client.ts
@@ -1,6 +1,11 @@
 import Shopify from '@shopify/shopify-api'
 import { GraphqlClient } from '@shopify/shopify-api/dist/clients/graphql/graphql_client'
 
+console.log(
+  'NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN',
+  process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN
+)
+
 export default new Shopify.Clients.Storefront(
   process.env.SHOPIFY_STOREFRONT_API_DOMAIN as string,
   process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN as string
