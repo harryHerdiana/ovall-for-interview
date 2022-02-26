3658a360dd4f177df419a3abcc10112c845e93d3
Harry Herdiana <harry.gamebot@gmail.com>
2022-02-27 02:15:23 +0700

    refactor the accordion on two components into Accordion component

--- component/FaqSection/FaqSection.tsx
+++ component/FaqSection/FaqSection.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import Icon from '@component/Icon'
-import Button from '@component/Button'
 import { IDatoAccordionItem } from '@modules/datocms/types'
-import { Disclosure } from '@headlessui/react'
+import Accordion from '@component/Accordion'
 
 type IFaqSectionProps = {
   faqButtonText: string
@@ -16,47 +14,12 @@ const FaqSection: React.FC<IFaqSectionProps> = ({
   faqSubtitle,
   faqTitle,
   items
-}) => {
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    })
-  }
-  return (
-    <section className="m-auto flex flex-col max-w-site md:w-3/4 xl:w-1/2 h-auto md:p-5 md:text-center my-10">
-      <div className="font-subtitleFont text-base px-5 md:px-0">{faqTitle}</div>
-      <div className="font-titleFont text-2xl  px-5 md:px-0">{faqSubtitle}</div>
-      <div className="my-10">
-        {items.map((item) => (
-          <Disclosure key={item.text}>
-            {({ open }) => (
-              <>
-                <hr />
-                <Disclosure.Button className="flex justify-between items-center w-full px-5 py-2 text-tiny font-medium text-left rounded-lg  ">
-                  <span className="font-subtitleFont uppercase text-base w-full md:w-3/4">
-                    {item.text}
-                  </span>
-                  <Icon
-                    src="/images/arrow-big.svg"
-                    className={`${open ? 'transform rotate-180' : ''}  w-8 h-8 `}
-                  />
-                </Disclosure.Button>
-                <Disclosure.Panel className="px-5 pt-4 pb-2 text-tiny font-textFont text-left">
-                  {item.body}
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
-        ))}
-      </div>
-      <div className="w-full md:w-1/2 m-auto  px-5 md:px-0">
-        <Button onClick={scrollToTop} buttonType="primary" type="button">
-          {faqButtonText}
-        </Button>
-      </div>
-    </section>
-  )
-}
+}) => (
+  <section className="m-auto flex flex-col max-w-site md:w-3/4 xl:w-1/2 h-auto md:p-5 md:text-center my-10">
+    <div className="font-subtitleFont text-base px-5 md:px-0">{faqTitle}</div>
+    <div className="font-titleFont text-2xl  px-5 md:px-0">{faqSubtitle}</div>
+    <Accordion items={items} buttonText={faqButtonText} />
+  </section>
+)
 
 export default FaqSection
