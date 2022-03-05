4b7a4e742890160d66c2be26b86292996dd5a203
Harry Herdiana <harry.gamebot@gmail.com>
2022-03-06 02:23:38 +0700

    start styling the slider

--- component/ProductStage/ProductSlideshow.tsx
+++ component/ProductStage/ProductSlideshow.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import ResponsiveImage from '@component/ResponsiveImage'
 import Slider from '@component/Slider'
 import { DatoCMSResponsiveImage } from '@modules/datocms/types'
+import Icon from '@component/Icon'
 
 import React, { useEffect, useState } from 'react'
 
@@ -20,13 +23,39 @@ const SliderItem: React.FC<ISliderItem> = ({ image, backgroundColor }) => (
   </div>
 )
 
+const NextArrow = (props) => {
+  const { onClick, className } = props
+  return (
+    <div onClick={onClick} className={className}>
+      <Icon src="/images/arrow-small.svg" className="h-10 w-10" />
+    </div>
+  )
+}
+
+const PrevArrow = (props) => {
+  const { onClick, className, style } = props
+  return (
+    <div
+      onClick={onClick}
+      className={className}
+      style={{ ...style, transform: 'rotate(180deg)', display: 'block' }}>
+      <Icon src="/images/arrow-small.svg" className="h-10 w-10" />
+    </div>
+  )
+}
+
+const setting = {
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />
+}
+
 const ProductSlideshow: React.FC<IProductSlideshow> = ({ items, variantItem }) => {
   const [allItems, setAllItems] = useState([])
   useEffect(() => {
     setAllItems([variantItem].concat(items))
   }, [variantItem])
   return (
-    <Slider className="w-full">
+    <Slider settings={setting} className="w-full">
       {allItems.map((item) => (
         <SliderItem image={item.image} key={item.id} backgroundColor={item.background} />
       ))}
