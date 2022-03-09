2412e72025ff119feb1ec13fe605a01590536e1d
Harry Herdiana <harry.gamebot@gmail.com>
2022-03-09 23:18:14 +0700

    adjust the spacing between slideshow and text on product stage

--- component/ProductStage/ProductStage.tsx
+++ component/ProductStage/ProductStage.tsx
@@ -47,13 +47,13 @@ const ProductStage: React.FC<IProductStageProps> = ({
   }
   console.log(productClaims)
   return (
-    <section className="gap-12 grid grid-cols-1 lg:grid-cols-2 md:p-4 xl:p-6 max-w-site mx-auto mb-12">
-      <div className="text-center md:text-left mx-auto md:pr-0 flex flex-wrap items-center justify-center w-full ">
+    <section className="grid grid-cols-1 lg:grid-cols-2 md:p-4 xl:p-12 max-w-site mx-auto mb-12">
+      <div className="text-center md:text-left mx-auto md:pr-0 flex flex-wrap items-center justify-center  w-full ">
         <div className="relative w-full h-full lg:w-5/6">
           <ProductSlideshow items={slideshowImages} variantItem={getVariantImageBySku(activeSku)} />
         </div>
       </div>
-      <div className="text-center lg:text-left text-black p-4  flex flex-col flex-wrap mx-auto">
+      <div className="text-center lg:text-left text-black  flex flex-col flex-wrap mx-auto ">
         <h1 className="w-full text-left mb-1">{product.title}</h1>
         <div className="text-left flex flex-row mb-3 mt-8">
           <span className=" self-center font-subtitleFont font-semibold text-2xl">
