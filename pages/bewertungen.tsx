840bf4a1bf448daf5c4c5b763d4591d4332d8a7e
Harry Herdiana <harry.gamebot@gmail.com>
2022-03-14 23:30:15 +0700

    added Transparenz section with hardoced

--- pages/bewertungen.tsx
+++ pages/bewertungen.tsx
@@ -7,6 +7,7 @@ import Newsletter from '@component/Newsletter'
 import ProductTeaser from '@component/ProductTeaser'
 import ProductReview from '@component/ProductReview'
 import GradientBanner from '@component/GradientBanner'
+import TransparenzSection from '@component/Transparenz'
 
 const RatingsPage: React.FC<IRatingsPage> = (props: IRatingsPage & IDefaultProps) => {
   const { newsletterSection, productTeaserSection, heroSection, productFeatureSection } = props
@@ -15,6 +16,11 @@ const RatingsPage: React.FC<IRatingsPage> = (props: IRatingsPage & IDefaultProps
     <Layout seoTags={props.seoTags} {...props.appProps}>
       <ProductReview />
       <GradientBanner mobileContentPlacement="bottom" contentPlacement="left" {...heroSection} />
+      <TransparenzSection
+        title="Woher kommen unsere Bewertungen?"
+        subtitle="transparenz"
+        body="Miaow then turn around and show you my bum relentlessly pursues moth only use one corner of the litter box or purr like a car engine oh yes, there is my human slave woman she does best pats ever that all i like about her hiss meow for bird bird bird bird bird bird human why take bird out i could have eaten that. One of these days i'm going to get that red dot, just you wait and see"
+      />
       <GradientBanner
         {...productFeatureSection}
         items={productFeatureSection.items}
