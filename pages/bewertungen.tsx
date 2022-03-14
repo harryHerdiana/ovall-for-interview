d4150a85bc5d2df585309d78b36734404e566553
Harry Herdiana <harry.gamebot@gmail.com>
2022-03-14 23:15:03 +0700

    added sections to pages/bewertungen.tsx

--- pages/bewertungen.tsx
+++ pages/bewertungen.tsx
@@ -6,13 +6,22 @@ import { IDefaultProps, IRatingsPage } from '@lib/types'
 import Newsletter from '@component/Newsletter'
 import ProductTeaser from '@component/ProductTeaser'
 import ProductReview from '@component/ProductReview'
+import GradientBanner from '@component/GradientBanner'
 
 const RatingsPage: React.FC<IRatingsPage> = (props: IRatingsPage & IDefaultProps) => {
-  const { newsletterSection, productTeaserSection } = props
+  const { newsletterSection, productTeaserSection, heroSection, productFeatureSection } = props
   console.log('props', props)
   return (
     <Layout seoTags={props.seoTags} {...props.appProps}>
       <ProductReview />
+      <GradientBanner mobileContentPlacement="bottom" contentPlacement="left" {...heroSection} />
+      <GradientBanner
+        {...productFeatureSection}
+        items={productFeatureSection.items}
+        mobileContentPlacement="bottom"
+        contentPlacement="left"
+      />
+
       <ProductTeaser {...productTeaserSection} />
       <Newsletter {...newsletterSection} />
     </Layout>
