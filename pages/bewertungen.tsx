df70690f044c996e1f63a0f7b49598b66d1e0db3
hellersson <dev@dominikheller.de>
2022-03-17 16:27:28 +0100

    test

--- pages/bewertungen.tsx
+++ pages/bewertungen.tsx
@@ -28,7 +28,7 @@ const RatingsPage: React.FC<IRatingsPage> = (props: IRatingsPage & IDefaultProps
       <TransparencySection
         title="Woher kommen unsere Bewertungen?"
         subtitle="transparenz"
-        body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        body="Lorem  Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
       />
       <GradientBanner
         {...productFeatureSection}
