96fba2a80fc6ac6426804e78ea9ae2955e451e03
hellersson <dev@dominikheller.de>
2022-03-17 17:16:36 +0100

    update gtm

--- modules/tracking/TrackingIframe.tsx
+++ modules/tracking/TrackingIframe.tsx
@@ -5,7 +5,7 @@ const TrackingIframe: React.FC = () => (
     {!process.env.NEXT_PUBLIC_DISABLE_TRACKING && (
       <noscript>
         <iframe
-          src="https://connect.ovallskincare.de/ns.html?id=GTM-STWMXQS"
+          src="https://connect.ovallskincare.de/ns.html?id=GTM-5TWMXQS"
           height="0"
           width="0"
           style={{ display: 'none', visibility: 'hidden' }}
--- modules/tracking/TrackingScript.tsx
+++ modules/tracking/TrackingScript.tsx
@@ -13,7 +13,7 @@ const TrackingScript: React.FC = () => {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://connect.ovallskincare.de/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-STWMXQS');
+          })(window,document,'script','dataLayer','GTM-5TWMXQS');
         `
       }}
       key="gtm"
