db7be583c3a352d550f2fcb67945f4b693882122
hellersson <dev@dominikheller.de>
2022-03-18 03:40:33 +0100

    fix hreflang typo

--- component/HrefLang/Hreflang.tsx
+++ component/HrefLang/Hreflang.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react'
 
 import { appUrl } from '@lib/utils'
@@ -6,13 +7,14 @@ interface IProps {
   en: string
   de: string
 }
+
 const HrefLang: React.FC<IProps> = ({ en, de }) => {
   const enUrl = `${appUrl()}${en}`
   const deUrl = `${appUrl()}${de}`
   return (
     <>
-      <link rel="alternate" hrefLang="en" href={enUrl} />
-      <link rel="alternate" hrefLang="de" href={deUrl} />
+      <link rel="alternate" hreflang="en" href={enUrl} />
+      <link rel="alternate" hreflang="de" href={deUrl} />
     </>
   )
 }
