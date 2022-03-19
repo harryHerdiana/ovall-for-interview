f2b77a8cea1867072f917ffc55342056bb876fe9
Harry Herdiana <harry.gamebot@gmail.com>
2022-03-20 03:37:33 +0700

    fixed social section font and spacing

--- component/SocialFeed/SocialFeed.tsx
+++ component/SocialFeed/SocialFeed.tsx
@@ -9,8 +9,8 @@ interface IProps {
 
 const TestimonialSection: React.FC<IProps> = ({ title, subtitle }) => (
   <div className="lg:w-4/5 mx-auto lg:text-center my-14 max-w-fullhd px-4 w-">
-    <div>{title.toUpperCase()}</div>
-    <h2 className="mb-5">{subtitle}</h2>
+    <div className="h3_element">{title.toUpperCase()}</div>
+    <h2 className="mb-5 mt-2">{subtitle.toUpperCase()}</h2>
 
     <Script src="https://apps.elfsight.com/p/platform.js" defer />
     <div className="elfsight-app-e7090e5a-3bc5-4675-92c8-56ad7ef98227" />
