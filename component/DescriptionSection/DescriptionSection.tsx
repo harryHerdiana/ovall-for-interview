db053dbae1f77a5c4b047dfd3b6d3768b4614d7f
hellersson <dev@dominikheller.de>
2022-03-20 16:24:33 +0100

    remove hardcoded video

--- component/DescriptionSection/DescriptionSection.tsx
+++ component/DescriptionSection/DescriptionSection.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 type IDescriptionSectionProps = {
   title: string
-  //   videoUrl?: string
+  videoUrl?: string
   text: string
 }
 
-const DescriptionSection: React.FC<IDescriptionSectionProps> = ({ title, text }) => (
+const DescriptionSection: React.FC<IDescriptionSectionProps> = ({ title, text, videoUrl }) => (
   <section className=" px-4 md:px-0 m-auto flex flex-col max-w-site md:w-3/4 xl:w-1/2 md:p-5 md:text-center mb-20 lg:items-center">
     <div
       style={{ lineHeight: 1.154 }}
@@ -14,14 +14,11 @@ const DescriptionSection: React.FC<IDescriptionSectionProps> = ({ title, text })
       {title}
     </div>
     <p className="mb-4">{text}</p>
-    <div className="h-190px lg:h-400px w-full">
-      <iframe
-        title={title}
-        className="w-full h-full"
-        //   src={videoUrl}
-        src="https://www.youtube.com/embed/Kwk_5CB2gx0"
-      />
-    </div>
+    {videoUrl && (
+      <div className="h-190px lg:h-400px w-full">
+        <iframe title={title} className="w-full h-full" src={videoUrl} />
+      </div>
+    )}
   </section>
 )
 
