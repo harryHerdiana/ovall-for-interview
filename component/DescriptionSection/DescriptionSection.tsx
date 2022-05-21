7e521670f9ddca315270f1c9d0cf5ca8097bf4cc
hellersson <dev@dominikheller.de>
2022-05-21 18:38:40 +0200

    add prop to description section to optionally show video

--- component/DescriptionSection/DescriptionSection.tsx
+++ component/DescriptionSection/DescriptionSection.tsx
@@ -4,15 +4,21 @@ type IDescriptionSectionProps = {
   title: string
   videoUrl?: string
   text: string
+  showVideo?: boolean
 }
 
-const DescriptionSection: React.FC<IDescriptionSectionProps> = ({ title, text, videoUrl }) => (
+const DescriptionSection: React.FC<IDescriptionSectionProps> = ({
+  title,
+  text,
+  videoUrl,
+  showVideo = false
+}) => (
   <section className="px-4 md:px-0 m-auto flex mb-100px lg:mb-80px flex-col max-w-content-sm md:w-3/4 xl:w-1/2  md:text-center  lg:items-center mt-12 lg:mt-40px">
     <h2 className="mb-2" style={{ letterSpacing: '0.1rem' }}>
       {title}
     </h2>
     <p>{text}</p>
-    {videoUrl && (
+    {videoUrl && showVideo && (
       <div className="h-190px lg:h-370px lg:w-700px">
         <iframe title={title} className="w-full h-full" src={videoUrl} />
       </div>
