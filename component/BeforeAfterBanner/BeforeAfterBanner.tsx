7bc1b9e476c52ed2c53a2493dad6ba965cf9b210
Harry Herdiana <harry.gamebot@gmail.com>
2022-05-27 19:15:25 +0700

    fixed mobile version

--- component/BeforeAfterBanner/BeforeAfterBanner.tsx
+++ component/BeforeAfterBanner/BeforeAfterBanner.tsx
@@ -28,14 +28,14 @@ const BeforeAfterBanner: React.FC<IBeforeAfterBannerProps> = ({
     contentPlacement="left"
     variantGradient={backgroundColor}>
     <div className="flex flex-col lg:grid lg:grid-cols-2 lg:items-center max-w-site lg:mx-auto">
-      <div className="flex-col items-start h-max justify-center w-full lg:w-full p-4 lg:p-0 -mt-16 lg:mt-0 lg:mr-8">
+      <div className="flex-col items-start h-max justify-center w-full lg:w-full p-4 lg:p-0 md:-mt-16 lg:mt-0 lg:mr-8">
         <div className="flex flex-col mb-4 lg:mb-0">
           <span className="kicker">{kicker}</span>
           <h2 className="mb-6 mt-2">{title}</h2>
-          <div className="grid grid-cols-3 gap-16">
+          <div className="grid md:grid-cols-3 md:gap-16 gap-5">
             {items.map((item) => (
-              <div>
-                <h3 className="text-xxl font-bold mt-4 mb-8">{item.percentage}</h3>
+              <div className="lg:block grid grid-cols-2">
+                <h3 className="text-xxl font-bold m-auto md:mt-4 md:mb-8">{item.percentage}</h3>
                 <p>{item.text}</p>
               </div>
             ))}
