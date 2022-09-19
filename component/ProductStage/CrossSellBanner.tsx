fb5a51e1cfd862225bbb2eed14db53a76894db7c
Harry Herdiana <harry.gamebot@gmail.com>
2022-09-19 11:39:21 +0700

    finished styling crossSellBanner v1

--- component/ProductStage/CrossSellBanner.tsx
+++ component/ProductStage/CrossSellBanner.tsx
@@ -31,41 +31,56 @@ const CrossSellBanner: React.FC<ICrossSellBannerProps> = ({
     shopContext.setShowCart(true)
   }
 
+  const ButtonGroup = () => (
+    <div className="sm:mt-20 mt-10 text-left">
+      {variant.quantityAvailable < 1 ? (
+        <AddToCartButton disabled buttonType="disabled" onClick={handleAddToCartClick}>
+          <span>{soldoutLabel.toUpperCase()}</span>
+        </AddToCartButton>
+      ) : (
+        <AddToCartButton buttonType="primary" onClick={handleAddToCartClick}>
+          {addToCartLabel}
+        </AddToCartButton>
+      )}
+      <p className="text-xs mt-2">{crossSellBanner.discountTerms}</p>
+    </div>
+  )
+
   return (
-    <div className="mt-16">
+    <div className="mt-16 lg:px-8">
       <GradientSquare variantGradient="violet">
-        <div>
-          <h3 className="text-center">{crossSellBanner.headline}</h3>
-          <div className="flex">
+        <div className="px-4 py-6">
+          <h2 className="sm:text-left text-center text-xl sm:text-2xl">
+            {crossSellBanner.headline}
+          </h2>
+          <div className="flex items-center">
             <ResponsiveImage image={crossSellBanner.productImage.responsiveImage} />
-            <div>
+            <div className="ml-6 mt-3 sm:mt-6 text-left">
               <ScrollableLink anchor="testimonial" className="no-underline text-black">
                 <ProductRating />
               </ScrollableLink>
-              <p>{productCleanser.title}</p>
+              <h3 className="mt-3">{productCleanser.title}</h3>
               <p>{crossSellBanner.productDescription}</p>
               <p>{crossSellBanner.productVolume}</p>
-              <p>
-                <span className="line-through font-bold text-sm font-subtitleFont">
+              <p className="mt-6 ">
+                <span className="line-through text-xl font-subtitleFont">
                   {/* {toEuro(variant.compareAtPriceV2.amount)} tempoary commented due to the discount price isn't available */}
-                  {toEuro(19.99)}
+                  {toEuro(25.99)}
                 </span>
-                {toEuro(variant.priceV2.amount)}
+                <span className="sm:ml-5 ml-3 font-bold text-2xl font-titleFont">
+                  {toEuro(variant.priceV2.amount)}
+                </span>
               </p>
-              <StructuredText data={crossSellBanner.discountInfo} />
-              <div className="mt-2">
-                {variant.quantityAvailable < 1 ? (
-                  <AddToCartButton disabled buttonType="disabled" onClick={handleAddToCartClick}>
-                    <span>{soldoutLabel.toUpperCase()}</span>
-                  </AddToCartButton>
-                ) : (
-                  <AddToCartButton buttonType="primary" onClick={handleAddToCartClick}>
-                    {addToCartLabel}
-                  </AddToCartButton>
-                )}
-                <p>{crossSellBanner.discountTerms}</p>
+              <div id="discount-info">
+                <StructuredText data={crossSellBanner.discountInfo} />
+              </div>
+              <div className="hidden sm:block">
+                <ButtonGroup />
               </div>
             </div>
+          </div>
+          <div className="block sm:hidden">
+            <ButtonGroup />
           </div>
         </div>
       </GradientSquare>
